15f72781feea6c0c706103a759f9c84db3754d6c
Allan Mullan <allan.mullan@informa.com>
2024-02-06 10:37:53 +0000

    chore: generalised naming for callable args

--- registry/decorator.py
+++ registry/decorator.py
@@ -4,16 +4,16 @@ from registry import query_registry, terminate_registry
 
 
 def register_query_function(resource_type: str) -> Callable:
-    def decorator(deletion_function: Callable[..., None]) -> Callable[..., None]:
-        query_registry[resource_type] = deletion_function
-        return deletion_function
+    def decorator(func: Callable[..., None]) -> Callable[..., None]:
+        query_registry[resource_type] = func
+        return func
 
     return decorator
 
 
 def register_terminate_function(resource_type: str) -> Callable:
-    def decorator(deletion_function: Callable[..., None]) -> Callable[..., None]:
-        terminate_registry[resource_type] = deletion_function
-        return deletion_function
+    def decorator(func: Callable[..., None]) -> Callable[..., None]:
+        terminate_registry[resource_type] = func
+        return func
 
     return decorator
