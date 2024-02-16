0a152e5bde5f5a051c6e68181afffa36e1926c6a
Allan Mullan <allan.mullan@informa.com>
2024-02-16 10:44:13 +0000

    feat: added batch function

--- utils/general.py
+++ utils/general.py
@@ -8,3 +8,8 @@ def check_delete(tags: dict):
     return not any(
         tag in tags and tags[tag].lower() == 'true' for tag in config.EXCEPTION_TAGS
     )
+
+
+def batch(iterable: list, n: int = 1):
+    for i in range(0, len(iterable), n):
+        yield iterable[i : i + n]
