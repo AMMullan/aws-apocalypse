04cc7d768bca7fb4203af9a6a57be5141ec8cffc
Allan Mullan <allan.mullan@informa.com>
2024-02-16 16:43:58 +0000

    feat: add DeleteResponse responses

--- services/acm.py
+++ services/acm.py
@@ -1,6 +1,9 @@
-from utils.general import check_delete
+import botocore.exceptions
+
+from registry import DeleteResponse
 from registry.decorator import register_query_function, register_terminate_function
 from utils.aws import boto3_paginate, boto3_tag_list_to_dict
+from utils.general import check_delete
 
 
 @register_query_function('CertificateManager::Certificate')
@@ -26,8 +29,19 @@ def query_acm_certificates(session, region) -> list[str]:
 
 
 @register_terminate_function('CertificateManager::Certificate')
-def remove_acm_certificates(session, region, resource_arns: list[str]) -> None:
+def remove_acm_certificates(
+    session, region, resource_arns: list[str]
+) -> DeleteResponse:
     acm = session.client('acm', region_name=region)
 
+    response = DeleteResponse()
+
     for cert_arn in resource_arns:
-        acm.delete_certificate(CertificateArn=cert_arn)
+        try:
+            acm.delete_certificate(CertificateArn=cert_arn)
+            response.success.append(cert_arn)
+        except botocore.exceptions.ClientError as e:
+            error_code = e.response['Error']['Code']
+            response.failures[error_code].append(cert_arn)
+
+    return response
